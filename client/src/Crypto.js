d6de733d55ee6059de6bbb4685b7082ce88fcf80
issaicjeon <issaic2000@gmail.com>
2021-05-08 23:21:43 -0400

    Functionality for every coin

--- client/src/Crypto.js
+++ client/src/Crypto.js
@@ -6,11 +6,23 @@ export default class Crypto extends React.Component {
     super();
     this.state = {
       price: 0,
+      symbol: "",
     };
   }
 
+  handleChange = (event) => {
+    this.setState({ symbol: event.target.value });
+  };
+
   buttonClick = () => {
-    axios.get("/crypto").then((response) => {
+    axios
+      .post("/symbol", {
+        symbol: this.state.symbol,
+      })
+      .then((response) => {
+        console.log(response);
+      });
+    axios.get("/price").then((response) => {
       this.setState({
         price: response.data.price,
       });
@@ -20,11 +32,20 @@ export default class Crypto extends React.Component {
   render() {
     return (
       <div>
-        What is the current price of DOGE?
+        Get the current price of any cryptocurrency:
         <div>
+          <form>
+            <input
+              value={this.state.value}
+              onChange={this.handleChange}
+            ></input>
+          </form>
           <button onClick={this.buttonClick}>Click me!</button>
         </div>
-        <div>The current price of DOGE is: {this.state.price}</div>
+        <div>
+          The current price of {this.state.symbol} is: {this.state.price}
+          {this.state.price === -1 && <div>Error: Symbol not found</div>}
+        </div>
       </div>
     );
   }
--- index.js
+++ index.js
@@ -8,18 +8,43 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-var symbol = "DOGE";
+var symbol = "ETH";
+var exchange = new ccxt.binance();
 
-app.get("/crypto", (req, res) => {
+app.post("/symbol", (req, res) => {
+  symbol = req.body.symbol;
+  console.log(symbol);
+});
+
+app.get("/price", (req, res) => {
   //access exchange and API
-  var exchange = new ccxt.binance();
+  var price = -1;
   (async () => {
     if (exchange.has["fetchTicker"]) {
       //get ticker info from symbol
-      var info = await exchange.fetchTicker(`${symbol}/USDT`);
-      var price = info.last;
-      console.log(price);
-      res.send({ price });
+      await exchange.loadMarkets();
+      if (exchange.symbols.includes(`${symbol}/USDT`)) {
+        var info = await exchange.fetchTicker(`${symbol}/USDT`);
+        price = info.last;
+        console.log(info);
+        res.send({ price });
+      } else res.send({ price });
     }
   })();
 });
+console.log("working");
+(async () => {
+  let sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+  if (exchange.has.fetchOHLCV) {
+    await sleep(exchange.rateLimit); // milliseconds
+    console.log(
+      await exchange.fetchOHLCV(
+        `${symbol}/USDT`,
+        "1m",
+        //TODO: Convert from GMT to EST
+        exchange.parse8601("2020-01-01T00:00:00Z"),
+        20
+      )
+    ); // one minute
+  }
+})();
